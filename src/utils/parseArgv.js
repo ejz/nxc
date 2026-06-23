adb04e1de54b71827843dbdb59c825dcfed9d07a
default <ejz@ya.ru>
2026-06-23 22:47:18 +0300

    _

--- src/utils/parseArgv.js
+++ src/utils/parseArgv.js
@@ -1,17 +1,59 @@
-export default function parseArgv(argv, cb) {
+import InvalidArgumentError from '../errors/InvalidArgumentError.js';
+import RequiredArgumentError from '../errors/RequiredArgumentError.js';
+
+export default function parseArgv(argv, dict) {
+    let ready = {};
+    dict = splitDict(dict);
     let i = 0;
     for (; i < argv.length; i++) {
         let arg = argv[i];
-        let next = argv[i + 1];
         if (arg === '--') {
             i++;
             break;
         }
-        if (arg.startsWith('-') && arg !== '-') {
-            i += cb(arg, next) ?? 0;
-            continue;
+        let isArg = arg.startsWith('-') && arg !== '-';
+        if (!isArg) {
+            break;
         }
-        break;
+        let desc = dict[arg.slice(1)];
+        if (desc === undefined) {
+            throw new InvalidArgumentError(arg);
+        }
+        let [key, cbVal] = desc;
+        let isMultiple = cbVal === Infinity;
+        cbVal = isMultiple ? true : cbVal;
+        if (typeof cbVal === 'boolean') {
+            let val = cbVal;
+            cbVal = (takeNext) => val ? takeNext() : true;
+        }
+        let takeNext = () => {
+            let next = argv[i + 1];
+            if (next === undefined) {
+                throw new RequiredArgumentError(arg);
+            }
+            i++;
+            return next;
+        };
+        let val = cbVal(takeNext);
+        if (isMultiple) {
+            let ref = ready[key] ??= [];
+            ref.push(val);
+        } else {
+            ready[key] = val;
+        }
     }
-    return argv.slice(i);
+    return [argv.slice(i), ready];
+}
+
+export function splitDict(dict) {
+    let split = {};
+    for (let k in dict) {
+        let v = dict[k];
+        let ps = k.split('|');
+        let c = ps[ps.length - 1];
+        ps.forEach((k) => {
+            split[k] = [c, v];
+        });
+    }
+    return split;
 }
