616849d770c76ceab5906351d7893ddbb5f8ec27
default <ejz@ya.ru>
2026-06-23 23:15:38 +0300

    _

--- bin/nxc.js
+++ bin/nxc.js
@@ -1,12 +1,12 @@
 import runner from './runner.js';
 import bootstrap from './bootstrap.js';
 import version from './commands/version.js';
-// import help from './commands/help.js';
+import help from './commands/help.js';
 // import execute from './commands/execute.js';
 // import compile from './commands/compile.js';
 
 const thisFile = __filename;
-const commands = {version/*, help, execute, compile*/};
+const commands = {version, help/*, execute, compile*/};
 
 bootstrap({thisFile, runner: (opts) => {
     opts.commands = commands;
